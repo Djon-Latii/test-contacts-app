12af74556ee883beac2da90ea427f56aca970deb
Djon <latiiodessa@gmail.com>
2021-05-05 14:55:14 +0300

    add table

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,53 +1,48 @@
-import { useEffect, useState } from "react";
+import { useContacts } from './useContacts'
+import { ContactsTable } from './ContactsTable';
+
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
-const useContacts = () => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
 
-    useEffect(() => {
-        const getContacts = async () => {
-            try {
-                setIsLoading(true);
-                const response = await fetch('https://randomuser.me/api/?results=200');
-                const { results, error } = await response.json();
-                if (error) {
-                    throw new Error(error);
-                }
-                setData(results);
-                setIsError(false);
-            } catch (error) {
-                setIsError(true);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        getContacts();
 
-    }, []);
-    return {
-        data,
-        isLoading,
-        isError
-    }
-}
+const useStyles = makeStyles((theme) => createStyles({
+    root: {
+        marginTop: theme.spacing(4),
+    },
+    headContainer: {
+        marginBottom: theme.spacing(3),
+    },
+}));
 
 export const Contacts = () => {
+    const classes = useStyles();
     const contacts = useContacts()
 
-    if (contacts.isLoading) {
-        return <div>...Loading</div>
-    }
-    if (contacts.isError) {
-        return <div>...Error</div>
-    }
+
 
     return <div>
-        <Container container spacing={3}>
-            <Grid item xs={12}>
-                <div> Contacts {contacts.data[0].name.first}</div>
-            </Grid> </Container>
+        <Container className={classes.root}>
+            <Grid container>
+                <Grid item xs={12} className={classes.headContainer}>
+                    <Typography variant="h3" component="h1">
+                        Contacts
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    {(() => {
+                        if (contacts.isLoading) {
+                            return <div>...Loading</div>
+                        }
+                        if (contacts.isError) {
+                            return <div>...Error</div>
+                        }
+                        return <ContactsTable data={contacts.data} />
+                    })()}
+                </Grid>
+            </Grid>
+        </Container>
     </div>
 }
